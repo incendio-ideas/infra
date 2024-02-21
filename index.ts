5d23942713ad4444f7e54398c14978ac6281c50b
patchwork-body <personal.gugfug@gmail.com>
2024-02-21 18:57:03 +0100

    feat: inc web service image version

--- index.ts
+++ index.ts
@@ -100,7 +100,7 @@ class IncendioChart extends Chart {
             containers: [
               {
                 name: "web",
-                image: "ghcr.io/incendio-ideas/web:0.0.2",
+                image: "ghcr.io/incendio-ideas/web:0.0.3",
                 ports: [{ containerPort: 8000 }],
               },
             ],
